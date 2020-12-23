c373f466cda7efe69a42673fb142db7777c33d10
Natanael Martinez Morales <natanael29032000@gmail.com>
2020-12-23 00:00:43 +0000

    Kata 3 - 1.0 Version

---
+++ src/kata3/HistogramDisplay.java
@@ -0,0 +1,50 @@
+package kata3;
+
+import java.awt.Dimension;
+import javax.swing.JPanel;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+
+public class HistogramDisplay extends ApplicationFrame {
+    
+    public HistogramDisplay() {
+        super("Histograma");
+        this.setContentPane(createPanel());
+        this.pack();
+    }
+    
+    public void execute(){
+        setVisible(true);
+    }
+    
+    private JPanel createPanel(){
+        ChartPanel chartPanel = new ChartPanel(createChart(createDataSet()));
+        chartPanel.setPreferredSize(new Dimension(500,400));
+        return chartPanel;
+    }
+    
+    private JFreeChart createChart(DefaultCategoryDataset dataSet) {
+        JFreeChart chart = ChartFactory.createBarChart("Histograma JFreeChart", 
+                                                        "Dominio email",
+                                                        "Nº de emails",
+                                                        dataSet,
+                                                        PlotOrientation.VERTICAL,
+                                                        false,
+                                                        false,
+                                                        rootPaneCheckingEnabled);
+        return chart;
+    }
+    
+    private DefaultCategoryDataset createDataSet(){
+        DefaultCategoryDataset dataSet = new DefaultCategoryDataset();
+        dataSet.addValue(2, "", "ulpgc.es");
+        dataSet.addValue(4, "", "gmail.es");
+        dataSet.addValue(6, "", "ull.es");
+        dataSet.addValue(1, "", "hotmail.com");
+        return dataSet;
+    }
+}
